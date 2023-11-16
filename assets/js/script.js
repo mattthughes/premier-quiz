0298dd8718f8e10070c6926c3bc3d32279a55180
mattthughes <>
2023-11-16 14:23:37 +0000

    Added extra comments and structured javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 import {
     questions
 } from './questions.js';
@@ -14,6 +13,7 @@ let instructionsTextElement = document.getElementById('instructions-text');
 let quizStartButton = document.getElementById('quiz-start-btn');
 let instructionStartBtn = document.getElementById('instructions-start-btn');
 let gameArea = document.getElementById('game-area');
+let quizHeading = document.getElementById('game-heading');
 let contactForm = document.getElementById('raffle');
 let submitButton = document.getElementById('submit-btn');
 let firstName = document.getElementById('first-name');
@@ -25,23 +25,18 @@ let thankYou = document.getElementById('thank-you');
 let backButton = document.getElementById('back-btn');
 let raffleInputs = document.getElementsByClassName('raffle-inputs');
 
-
 // Initalise the Quiz //
-
 let score = 0;
 let question = 0;
 let answer = 0;
 let currentQuestionIndex = 0;
-
 hideInstructions();
 hideAnswers();
-
 instructionsButton.addEventListener('click', showInstructions);
 quizStartButton.addEventListener('click', runGame);
 instructionStartBtn.addEventListener('click', runGame);
 
 /**Hidding  key elements */
-
 function hideInstructions() {
     instructionsTextElement.classList.add('hide');
     gameArea.classList.add('hide');
@@ -51,7 +46,6 @@ function hideInstructions() {
 }
 
 /**Hidding  key elements */
-
 function hideAnswers() {
     answerButtons.classList.add('hide');
     questionElement.classList.add('hide');
@@ -59,48 +53,36 @@ function hideAnswers() {
 }
 
 /**Hidding  key elements */
-
 function hideElements() {
-
     instructionsButton.classList.add('hide');
     quizIntro.classList.add('hide');
     quizElement.classList.add('hide');
-
 }
 
 /**Hidding and unhiding key elements */
-
 function showInstructions() {
     instructionsTextElement.classList.remove('hide');
     instructionsTextElement.classList.add('show', 'quiz-instructions');
     hideElements();
 }
 
-
 /** Hidding and unhiding key elements while also using the start quiz function to start when the start button is clicked */
-
 function runGame() {
     questionElement.classList.remove('hide');
     questionElement.classList.add('show');
     instructionsTextElement.classList.remove('show');
     hideElements();
     hideInstructions();
-
     answerButtons.classList.remove('hide');
     answerButtons.classList.add('show');
-
     nextButton.classList.remove('hide');
     nextButton.classList.add('add');
-
     gameArea.classList.remove('hide');
     gameArea.classList.add('show');
-
     startQuiz();
-
 }
 
 /**Setting up quiz functionality by setting default parameters */
-
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
@@ -108,13 +90,14 @@ function startQuiz() {
     showQuestions();
 }
 
-/* This function is reseting the state and getting the current question by using the object questions and the currentquestion index 
-previously declared to get the question number is done by the current question index adding 1 and then setting the inner next of the question
-to grab the question from the object, after this using a for each loop to the questions answers by creating a new element button for 
-the answers and changing the button text to the answers.text which is the answers in the array answers using the append child 
-method to move the button then finally setting up the correct answer functionality by checking if the answer.correct is true to 
-set the button data to answers.correct I then create an event listener with a click parameter and a select answer function. */
-
+/** Get current question which is questions array and the current question index 
+ * Set question number to the question index and add one
+ * set the question element text to the number of the question and the current question
+ * use a for each loop for the answers in the array to create the answer buttons using append 
+ * child to add new node. if answers is correct change button to correct. 
+ * add event listener which is a click method and select answer function using the select answer 
+ * function
+ */
 function showQuestions() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
@@ -125,58 +108,46 @@ function showQuestions() {
         button.innerText = answers.text;
         button.classList.add('answer-btn');
         answerButtons.appendChild(button);
-
         if (answers.correct) {
             button.dataset.correct = answers.correct;
         }
-
         button.addEventListener("click", selectAnswer);
-
     });
-
 }
 
-
-/* This function is first creating a variable called select button with the constant key word which cannot be changed then creating
-another varible which is checking if the select button is the variable in use. After its checkig if the question is correct to 
-add on score by 1 and to add the class correct which is changing the colour of the button and if the answer is wrong 
-to add the incorrect class, next accessing the next button once an answer has been selected and setting this to a block 
-level element after this a for each loop is getting the array answers and the children which is the text and the true 
-and false values once this is done to disable all buttons once an option has been clicked using the . disabled method
-and finally I add an event listener using the click and empty function parameter which simply checks if the current question
-index is less than the length of the object questions if it is you show the next question or start the quiz.* */
+/**First creates a variable select button then
+ * another variable checking if the select button is in use. 
+ * then add one score by 1 if correct and changing button green
+ * and if wrong changing button red after a for each loop is getting 
+ * the array answers and disabling all buttons when an answer has been 
+ * selected then finally checking if the question index is less than 
+ * the length of the array if it is show another question if not 
+ * start quiz.
+ */
 
 function selectAnswer(event) {
     const selectButton = event.target;
     const isCorrect = selectButton.dataset.correct === "true";
-
     if (isCorrect) {
         score++;
         selectButton.classList.add("correct");
     } else {
         selectButton.classList.add("incorrect");
-
     }
-
     nextButton.style.display = 'block';
     nextButton.classList.add('nxt-btn');
-
-
     Array.from(answerButtons.children).forEach(button => {
         button.disabled = true;
     });
-
 }
-
 nextButton.addEventListener("click", () => {
-
     if (currentQuestionIndex < questions.length) {
         showNextQuestion();
     } else {
         startQuiz();
     }
-
 });
+
 /** This function is adding onto the current question index and showing if the current question index is less 
  * than the questions in the array to show questions to get another question and answers from 
  * the question object otherwise show the results function.
@@ -189,6 +160,7 @@ function showNextQuestion() {
         showResults();
     }
 }
+
 /** This function is first using the reset state and then checking what the score is
  * then i am changing the question element space by using the inner html property to change the 
  * html and doing the same for the next button and setting this to a block level element. with the last option allowing the user to go to 
@@ -212,27 +184,20 @@ function showResults() {
         nextButton.style.display = 'block';
         nextButton.innerHTML = 'next';
         nextButton.addEventListener('click', joinRaffle);
-
     }
-
 }
+
 //** This function is first removing the hide class and adding the show class to make this html visible which was previously hidden 
 // After this the function is hidding the question and answer elements to showcase the correct elements this will only be shown if a user gets ever answer correct **/
-
-
 function joinRaffle() {
-
     contactForm.classList.remove('hide');
     contactForm.classList.add('show');
-
     questionElement.classList.remove('show');
     questionElement.classList.add('hide');
-
     answerButtons.classList.remove('show');
     answerButtons.classList.add('hide');
-
     contactForm.classList.add('raffle');
-
+    quizHeading.classList.add('hide');
     submitButton.addEventListener('click', raffleEnd);
 }
 
@@ -243,23 +208,18 @@ function raffleEnd(event) {
     thankYou.classList.remove('hide');
     thankYou.classList.add('show');
 
-
-
     if (favouriteTeam.value === 'None') {
         thankYou.classList.add('thank-you-none');
         thankYouText.innerHTML = `Thank you for taking part in the quiz ${firstName.value} ${surname.value} , If you would like to take part again in the quiz just simply press the back button.`;
     } else {
 
-    thankYouText.innerHTML = `Thank you for entering the raffle ${firstName.value} ${surname.value} , 
-    We have recieved your message and if you are successful ${favouriteTeam.value} will be in touch. 
+        thankYouText.innerHTML = `Thank you for entering the raffle ${firstName.value} ${surname.value} , 
+    We have recieved your message and if you are successful ${favouriteTeam.value} will be in touch with the ${emailAddress.value} you provided,. 
     If you would like to take the quiz again simply just press the back button`;
-    thankYou.classList.add('thank-you');
-
+        thankYou.classList.add('thank-you');
     }
-
     backButton.classList.remove('hide');
     backButton.classList.add('show');
-
     event.preventDefault();
 }
 
